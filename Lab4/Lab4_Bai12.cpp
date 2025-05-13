a1b553accecd1f16b2999f93892e88d8f800c80c
Nam Phuong <157980092+num153@users.noreply.github.com>
2025-05-13 07:14:21 +0700

    Add files via upload

---
+++ Lab4/Lab4_Bai12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cout << "Nhap chuoi: ";
+	getline(cin, s);
+	reverse(s.begin(), s.end());
+	cout << "Chuoi sau khi dao nguoc: " << s << endl;
+	return 0;
+}
